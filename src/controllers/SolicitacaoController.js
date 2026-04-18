1e1a249301f0b16a15297317bba91be1c84752c6
HIRATA Sergio <sergio.hirata@amaris.com>
2026-04-18 13:33:03 -0300

    ajuste no bloqueio de novas apometrias

--- src/controllers/SolicitacaoController.js
+++ src/controllers/SolicitacaoController.js
@@ -51,15 +51,37 @@ exports.criarSolicitacaoComCadastro = async (req, res) => {
             });
         }
 
+        const dataSolicitacao = new Date(`${dados.data}T00:00:00-03:00`);
         const ultimoAtendimento = await Atendimento.findOne({
             cpf_assistido: dados.cpf_assistido,
             tipo: tipoParaBusca
         }).sort({ data: -1 }).lean();
 
         if (ultimoAtendimento) {
-            const dataSolicitacao = new Date(`${dados.data}T00:00:00-03:00`);
+            const [totalApometriasHistoricas, totalPassesHistoricos, totalOutrosAtendimentos] = await Promise.all([
+                Atendimento.countDocuments({
+                    cpf_assistido: dados.cpf_assistido,
+                    tipo: 'apometria'
+                }),
+                Atendimento.countDocuments({
+                    cpf_assistido: dados.cpf_assistido,
+                    tipo: 'passe'
+                }),
+                Atendimento.countDocuments({
+                    cpf_assistido: dados.cpf_assistido,
+                    tipo: { $nin: ['apometria', 'passe'] }
+                })
+            ]);
+
+            const diasDeBloqueio =
+                totalApometriasHistoricas === 1 &&
+                totalPassesHistoricos === 1 &&
+                totalOutrosAtendimentos === 0
+                    ? 90
+                    : 27;
+
             const dataLiberacao = new Date(ultimoAtendimento.data);
-            dataLiberacao.setDate(dataLiberacao.getDate() + 90);
+            dataLiberacao.setDate(dataLiberacao.getDate() + diasDeBloqueio);
 
             if (dataSolicitacao < dataLiberacao) {
                 return res.json({
